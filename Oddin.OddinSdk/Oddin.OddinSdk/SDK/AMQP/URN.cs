add5cf9bb79c2a95240d7b69ea025f664ba810bd
Jiří Soukup <giri.soukup@gmail.com>
2021-06-30 16:17:00 +0200

    + dummy URN

---
+++ Oddin.OddinSdk/Oddin.OddinSdk/SDK/AMQP/URN.cs
@@ -0,0 +1,15 @@
+﻿namespace Oddin.OddinSdk.SDK.AMQP
+{
+    public class URN
+    {
+        // INFO: for the time being just a dummy class,
+        // can be made more complex if needed
+
+        public string Urn { get; }
+
+        public URN(string urn)
+        {
+            Urn = urn;
+        }
+    }
+}
--- Oddin.OddinSdk/Oddin.OddinSdk/SDK/API/Entities/Abstractions/ISportEvent.cs
+++ Oddin.OddinSdk/Oddin.OddinSdk/SDK/API/Entities/Abstractions/ISportEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oddin.OddinSdk.SDK.AMQP;
+using System;
 using System.Globalization;
 using System.Threading.Tasks;
 
@@ -9,11 +10,10 @@ namespace Oddin.OddinSdk.SDK.API.Entities.Abstractions
     /// </summary>
     public interface ISportEvent
     {
-        // TODO: replace string with URN?
         /// <summary>
         /// Gets a <see cref="URN"/> uniquely identifying the sport event associated with the current instance
         /// </summary>
-        string Id { get; }
+        URN Id { get; }
 
         /// <summary>
         /// Asynchronously gets the name of the sport event
@@ -22,12 +22,11 @@ namespace Oddin.OddinSdk.SDK.API.Entities.Abstractions
         /// <returns>Return a name of the race, or match</returns>
         Task<string> GetNameAsync(CultureInfo culture);
 
-        // TODO: replace string with URN?
         /// <summary>
         /// Asynchronously gets a <see cref="URN"/> uniquely identifying the sport associated with the current instance
         /// </summary>
         /// <returns>Returns a <see cref="URN"/> uniquely identifying the sport associated with the current instance</returns>
-        Task<string> GetSportIdAsync();
+        Task<URN> GetSportIdAsync();
 
         /// <summary>
         /// Asynchronously gets a <see cref="DateTime" /> instance specifying for when the sport event associated with the current instance is scheduled or a null reference if the value is not known
@@ -47,11 +46,10 @@ namespace Oddin.OddinSdk.SDK.API.Entities.Abstractions
         /// <returns>A <see cref="bool?"/> specifying if the start time to be determined is set for the associated sport event.</returns>
         Task<bool?> GetStartTimeTbdAsync();
 
-        // TODO: replace string with URN?
         /// <summary>
         /// Asynchronously gets a <see cref="URN"/> specifying the replacement sport event for the associated sport event.
         /// </summary>
         /// <returns>A <see cref="URN"/> specifying the replacement sport event for the associated sport event.</returns>
-        Task<string> GetReplacedByAsync();
+        Task<URN> GetReplacedByAsync();
     }
 }
