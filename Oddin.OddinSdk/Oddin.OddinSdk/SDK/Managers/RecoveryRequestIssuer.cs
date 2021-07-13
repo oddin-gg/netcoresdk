500fcf103ee13d512f729997f6b575bddd82c58a
Jiří Soukup <giri.soukup@gmail.com>
2021-07-13 11:48:22 +0200

    + RecoveryRequestIssuer

---
+++ Oddin.OddinSdk/Oddin.OddinSdk/SDK/Managers/RecoveryRequestIssuer.cs
@@ -0,0 +1,40 @@
+﻿using Oddin.OddinSdk.SDK.AMQP;
+using Oddin.OddinSdk.SDK.API.Abstractions;
+using Oddin.OddinSdk.SDK.API.Entities.Abstractions;
+using Oddin.OddinSdk.SDK.FeedConfiguration;
+using Oddin.OddinSdk.SDK.Managers.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace Oddin.OddinSdk.SDK.Managers
+{
+    internal class RecoveryRequestIssuer : IEventRecoveryRequestIssuer
+    {
+        private readonly IApiClient _apiClient;
+        private readonly int _nodeId;
+
+        public RecoveryRequestIssuer(IApiClient apiClient, IOddsFeedConfiguration config)
+        {
+            if (apiClient is null)
+                throw new ArgumentNullException(nameof(apiClient));
+
+            if (config is null)
+                throw new ArgumentNullException(nameof(config));
+
+            _apiClient = apiClient;
+
+            // TODO: load node id from config !!!!!!!!!!!!!!!!!!!!!!
+            _nodeId = 0;
+        }
+
+        public Task<long> RecoverEventMessagesAsync(IProducer producer, URN eventId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<long> RecoverEventStatefulMessagesAsync(IProducer producer, URN eventId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
